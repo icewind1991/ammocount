b3e6c55cd9688878ee4a1fd9eedbd22604aefdff
Robin Appelman <robin@icewind.nl>
2022-04-07 20:45:10 +0200

    rework weapon tracking

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use tf_demo_parser::demo::gameevent_gen::GameEvent;
 use tf_demo_parser::demo::message::packetentities::{EntityId, PacketEntity};
 use tf_demo_parser::demo::message::Message;
 use tf_demo_parser::demo::packet::datatable::{
-    ParseSendTable, SendTableName, ServerClass, ServerClassName,
+    ClassId, ParseSendTable, SendTableName, ServerClass, ServerClassName,
 };
 use tf_demo_parser::demo::packet::stringtable::StringTableEntry;
 use tf_demo_parser::demo::parser::gamestateanalyser::UserId;
@@ -22,42 +22,23 @@ use tf_demo_parser::{DemoParser, ReadResult, Stream};
 fn main() -> Result<(), MainError> {
     let mut args = args();
     let bin = args.next().unwrap();
-    let (path, steam_id, start, end, clipsize1, clipsize2) = if let (
-        Some(path),
-        Some(steam_id),
-        Some(start),
-        Some(end),
-        Some(clipsize1),
-        Some(clipsize2),
-    ) = (
-        args.next(),
-        args.next(),
-        args.next(),
-        args.next(),
-        args.next(),
-        args.next(),
-    ) {
+    let (path, steam_id, start, end) = if let (Some(path), Some(steam_id), Some(start), Some(end)) =
+        (args.next(), args.next(), args.next(), args.next())
+    {
         (
             path,
             SteamID::try_from(steam_id.as_str()).expect("invalid steam id"),
             start.parse().expect("invalid start tick"),
             end.parse().expect("invalid end tick"),
-            clipsize1.parse().expect("invalid end tick"),
-            clipsize2.parse().expect("invalid end tick"),
         )
     } else {
-        println!(
-                "usage: {} <demo> <steam id> <start tick> <end tick> <clipsize primary> <clipsize secondary>",
-                bin
-            );
+        println!("usage: {} <demo> <steam id> <start tick> <end tick>", bin);
         return Ok(());
     };
     let file = fs::read(&path)?;
     let demo = Demo::new(&file);
-    let parser = DemoParser::new_all_with_analyser(
-        demo.get_stream(),
-        AmmoCountAnalyser::new(steam_id, [clipsize1, clipsize2]),
-    );
+    let parser =
+        DemoParser::new_all_with_analyser(demo.get_stream(), AmmoCountAnalyser::new(steam_id));
     let (header, state) = parser.parse()?;
     let time_per_tick = header.duration / header.ticks as f32;
     let ammo_path = format!("{}_ammo.txt", path);
@@ -68,30 +49,14 @@ fn main() -> Result<(), MainError> {
     writeln!(&mut ammo_out, "txt = []")?;
     writeln!(&mut health_out, "txt = []")?;
     let mut last_frame = 0;
-    for (tick, clip, weapon_index, health) in state
+    for (tick, clip, max_clip, health) in state
         .into_iter()
         .filter(|(tick, _, _, _)| *tick >= start && *tick <= end)
     {
         let frame = ((tick - start) as f32 * time_per_tick * 60.0) as i32;
-        let clip_size = if weapon_index == 0 {
-            clipsize1
-        } else if weapon_index == 1 {
-            clipsize2
-        } else {
-            0
-        };
         for frame in last_frame..frame {
-            if clip_size == 0 {
-                println!("txt[{}] = \"\";", frame);
-                writeln!(&mut ammo_out, "txt[{}] = \"\";", frame)?;
-            } else {
-                println!("txt[{}] = \"{}/{}\";", frame, clip, clip_size);
-                writeln!(
-                    &mut ammo_out,
-                    "txt[{}] = \"{}/{}\";",
-                    frame, clip, clip_size
-                )?;
-            }
+            println!("txt[{}] = \"{}/{}\";", frame, clip, max_clip);
+            writeln!(&mut ammo_out, "txt[{}] = \"{}/{}\";", frame, clip, max_clip)?;
             writeln!(&mut health_out, "txt[{}] = \"{}\";", frame, health)?;
         }
         last_frame = frame;
@@ -100,15 +65,13 @@ fn main() -> Result<(), MainError> {
 }
 
 pub struct AmmoCountAnalyser {
-    output: Vec<(u32, u16, u8, u16)>,
-    current_clip: [u16; 3],
-    max_clip: [u16; 2],
+    output: Vec<(u32, u16, u16, u16)>,
+    max_clip: FnvHashMap<EntityId, u16>,
+    clip: FnvHashMap<EntityId, u16>,
     current_health: u16,
     class_names: Vec<ServerClassName>,
-    // indexed by ClassId
     local_player_id: EntityId,
     local_user_id: UserId,
-    local_weapons_ids: [i64; 3],
     outer_map: FnvHashMap<i64, EntityId>,
     active_weapon: i64,
     start_tick: u32,
@@ -118,7 +81,7 @@ pub struct AmmoCountAnalyser {
 }
 
 impl MessageHandler for AmmoCountAnalyser {
-    type Output = Vec<(u32, u16, u8, u16)>;
+    type Output = Vec<(u32, u16, u16, u16)>;
 
     fn does_handle(_message_type: MessageType) -> bool {
         true
@@ -183,41 +146,57 @@ const OUTER_CONTAINER_PROP: SendPropIdentifier =
 const ACTIVE_WEAPON_PROP: SendPropIdentifier =
     SendPropIdentifier::new("DT_BaseCombatCharacter", "m_hActiveWeapon");
 const HEALTH_PROP: SendPropIdentifier = SendPropIdentifier::new("DT_BasePlayer", "m_iHealth");
+#[allow(dead_code)]
 const UBER_CHARGE_PROP: SendPropIdentifier =
     SendPropIdentifier::new("DT_TFWeaponMedigunDataNonLocal", "m_flChargeLevel");
+#[allow(dead_code)]
 const UBER_CHARGE_PROP_LOCAL: SendPropIdentifier =
     SendPropIdentifier::new("DT_LocalTFWeaponMedigunData", "m_flChargeLevel");
 
+#[allow(dead_code)]
 const WEAPON1_ID_PROP: SendPropIdentifier = SendPropIdentifier::new("m_hMyWeapons", "000");
+#[allow(dead_code)]
 const WEAPON2_ID_PROP: SendPropIdentifier = SendPropIdentifier::new("m_hMyWeapons", "001");
+#[allow(dead_code)]
 const WEAPON3_ID_PROP: SendPropIdentifier = SendPropIdentifier::new("m_hMyWeapons", "002");
 
+const OUTER_NULL: i64 = 0x1FFFFF;
+
 impl AmmoCountAnalyser {
-    pub fn new(steam_id: SteamID, max_clip: [u16; 2]) -> Self {
+    pub fn new(steam_id: SteamID) -> Self {
         AmmoCountAnalyser {
             output: Default::default(),
-            current_clip: Default::default(),
             class_names: Default::default(),
             local_player_id: Default::default(),
-            local_weapons_ids: Default::default(),
             outer_map: Default::default(),
             active_weapon: 0,
             start_tick: 0,
             last_tick: 0,
             prop_names: Default::default(),
             local_steam_id: steam_id,
-            max_clip,
+            max_clip: Default::default(),
+            clip: Default::default(),
             local_user_id: 0u32.into(),
             current_health: 0,
         }
+    }
+
+    #[allow(dead_code)]
+    fn server_class(&self, id: ClassId) -> &str {
+        self.class_names[u16::from(id) as usize].as_str()
+    }
+
+    #[allow(dead_code)]
+    fn prop_name(&self, id: SendPropIdentifier) -> String {
+        let (t, n) = self.prop_names.get(&id).unwrap();
+        format!("{}.{}", t, n)
     }
 
     fn handle_event(&mut self, event: &GameEvent) {
         match event {
             GameEvent::PlayerSpawn(spawn) => {
                 if UserId::from(spawn.user_id) == self.local_user_id {
-                    self.current_clip[0] = self.max_clip[0];
-                    self.current_clip[1] = self.max_clip[1];
+                    self.clip = self.max_clip.clone();
                 }
             }
             _ => {}
@@ -228,73 +207,44 @@ impl AmmoCountAnalyser {
         if self.start_tick == 0 {
             self.start_tick = tick;
         }
-        self.handle_attribute_container(entity);
 
         for prop in entity.props() {
             match prop.value {
-                SendPropValue::Integer(id) if id != 2097151 => {
-                    if entity.entity_index == self.local_player_id {
-                        if prop.identifier == WEAPON1_ID_PROP {
-                            self.local_weapons_ids[0] = id;
-                        } else if prop.identifier == WEAPON2_ID_PROP {
-                            self.local_weapons_ids[1] = id;
-                        } else if prop.identifier == WEAPON3_ID_PROP {
-                            self.local_weapons_ids[2] = id;
-                        } else if prop.identifier == ACTIVE_WEAPON_PROP {
-                            self.active_weapon = id;
-                        } else if prop.identifier == HEALTH_PROP {
-                            self.current_health = id as u16;
+                SendPropValue::Integer(value) if value != OUTER_NULL => match prop.identifier {
+                    ACTIVE_WEAPON_PROP if entity.entity_index == self.local_player_id => {
+                        self.active_weapon = value;
+                    }
+                    HEALTH_PROP if entity.entity_index == self.local_player_id => {
+                        self.current_health = value as u16;
+                    }
+                    OUTER_CONTAINER_PROP => {
+                        if !self.outer_map.contains_key(&value) {
+                            self.outer_map.insert(value, entity.entity_index);
                         }
                     }
-                }
+                    CLIP_PROP => {
+                        let clip_max = self.max_clip.entry(entity.entity_index).or_default();
+                        *clip_max = (*clip_max).max(value as u16);
+                        self.clip.insert(entity.entity_index, value as u16);
+                    }
+                    _ => {}
+                },
                 _ => {}
-            }
-            for i in 0..3 {
-                if let Some(weapon_entity_id) = self.outer_map.get(&self.local_weapons_ids[i]) {
-                    if entity.entity_index == *weapon_entity_id {
-                        if prop.identifier == CLIP_PROP {
-                            if let SendPropValue::Integer(value) = prop.value {
-                                let value = value - 1; //clip size starts from 1
-                                self.current_clip[i] = value as _;
-                            } else {
-                                panic!("{}", prop.value)
-                            }
-                        }
-                    }
-                }
             }
         }
 
         if tick != self.last_tick && tick > self.start_tick {
-            for i in 0..3 {
-                if self.local_weapons_ids[i] == self.active_weapon {
+            if let Some(active_weapon) = self.outer_map.get(&self.active_weapon) {
+                if self.clip.contains_key(active_weapon) {
                     self.output.push((
                         tick - self.start_tick,
-                        self.current_clip[i],
-                        i as u8,
+                        self.clip[active_weapon].saturating_sub(1),
+                        self.max_clip[active_weapon].saturating_sub(1),
                         self.current_health,
                     ));
                 }
             }
             self.last_tick = tick;
-        }
-    }
-
-    fn handle_attribute_container(&mut self, entity: &PacketEntity) {
-        let mut out = 2097151;
-        for prop in entity.props() {
-            if prop.identifier == OUTER_CONTAINER_PROP {
-                if let SendPropValue::Integer(outer_id) = prop.value {
-                    if outer_id != 2097151 {
-                        out = outer_id;
-                    }
-                }
-            }
-        }
-        if out != 2097151 {
-            if !self.outer_map.contains_key(&out) {
-                self.outer_map.insert(out, entity.entity_index);
-            }
         }
     }
 
