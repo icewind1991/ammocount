33920076b0a907b0ccd20e432bc9214ce4d9f2f7
Robin Appelman <robin@icewind.nl>
2022-07-11 17:09:12 +0200

    flake cross compile

--- flake.nix
+++ flake.nix
@@ -16,9 +16,30 @@
       pkgs = import nixpkgs {
         inherit system overlays;
       };
+
+      pkgs-cross-mingw = import nixpkgs {
+        crossSystem = {
+          config = "x86_64-w64-mingw32";
+        };
+        inherit system overlays;
+      };
+      mingw_w64_cc = pkgs-cross-mingw.stdenv.cc;
+      mingw_w64 = pkgs-cross-mingw.windows.mingw_w64;
+      windows = pkgs-cross-mingw.windows;
     in rec {
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [rust-bin.beta.latest.default bacon cargo-cross];
+        nativeBuildInputs = with pkgs; [
+          (rust-bin.stable.latest.default.override {
+            targets = [ "x86_64-pc-windows-gnu" ];
+          })
+          bacon
+          mingw_w64_cc
+        ];
+        depsBuildBuild = [ pkgs.wine64 ];
+        buildInputs = [ windows.pthreads ];
+
+        CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${mingw_w64_cc.targetPrefix}cc";
+        CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = "wine64";
       };
     });
 }
