d970473b60fa0da8a7ddbe0f46328f46d40017f9
Robin Appelman <robin@icewind.nl>
2022-07-11 16:36:47 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,24 @@
 {
   inputs = {
+    nixpkgs.url = "nixpkgs/release-22.05";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
+    rust-overlay,
   }:
     utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages."${system}";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
     in rec {
-      # `nix develop`
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rustc cargo bacon];
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [rust-bin.beta.latest.default bacon cargo-cross];
       };
     });
 }
